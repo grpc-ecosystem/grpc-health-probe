115bd851b93ba0a87d6e8f302aa1e34f12eddf79
Ahmet Alp Balkan <ahmetb@google.com>
2018-09-18 20:20:19 -0700

    add -tls* flags
    Signed-off-by: Ahmet Alp Balkan <ahmetb@google.com>

--- main.go
+++ main.go
@@ -15,20 +15,31 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"flag"
+	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"os/signal"
 	"time"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 	healthpb "google.golang.org/grpc/health/grpc_health_v1"
 )
 
 var (
-	flAddr        string
-	flConnTimeout time.Duration
-	flRPCTimeout  time.Duration
+	flAddr          string
+	flConnTimeout   time.Duration
+	flRPCTimeout    time.Duration
+	flTLS           bool
+	flTLSNoVerify   bool
+	flTLSCACert     string
+	flTLSClientCert string
+	flTLSClientKey  string
+	flTLSServerName string
 )
 
 const (
@@ -44,25 +55,99 @@ const (
 
 func init() {
 	log.SetFlags(0)
-
-	// TODO(ahmetb) add flags for -insecure and authentication
+	flag.StringVar(&flAddr, "addr", "", "(required) tcp host:port to connect")
+	// timeouts
 	flag.DurationVar(&flConnTimeout, "connect-timeout", time.Second, "timeout for establishing connection")
 	flag.DurationVar(&flRPCTimeout, "rpc-timeout", time.Second, "timeout for health check rpc")
-	flag.StringVar(&flAddr, "addr", "", "(required) tcp host:port to connect")
+	// tls settings
+	flag.BoolVar(&flTLS, "tls", false, "use TLS (default: false, INSECURE plaintext transport)")
+	flag.BoolVar(&flTLSNoVerify, "tls-no-verify", false, "(with -tls) don't verify the certificate (INSECURE) presented by the server (default: false)")
+	flag.StringVar(&flTLSCACert, "tls-ca-cert", "", "(with -tls, optional) file containing trusted certificates for verifying server")
+	flag.StringVar(&flTLSClientCert, "tls-client-cert", "", "(with -tls, optional) client certificate for authenticating to the server (requires -tls-client-key)")
+	flag.StringVar(&flTLSClientKey, "tls-client-key", "", "(with -tls) client private key for authenticating to the server (requires -tls-client-cert)")
+	flag.StringVar(&flTLSServerName, "tls-server-name", "", "(with -tls) override the server name used to verify server certificates")
+
 	flag.Parse()
 
-	if flAddr == "" {
-		log.Println("-addr not specified")
+	argError := func(s string, v ...interface{}) {
+		log.Printf("error: "+s, v...)
 		os.Exit(StatusInvalidArguments)
+	}
+
+	if flAddr == "" {
+		argError("-addr not specified")
 	}
 	if flConnTimeout <= 0 {
-		log.Printf("-connect-timeout must be greater than zero (specified: %v)", flConnTimeout)
-		os.Exit(StatusInvalidArguments)
+		argError("-connect-timeout must be greater than zero (specified: %v)", flConnTimeout)
 	}
 	if flRPCTimeout <= 0 {
-		log.Printf("-rpc-timeout must be greater than zero (specified: %v)", flRPCTimeout)
-		os.Exit(StatusInvalidArguments)
+		argError("-rpc-timeout must be greater than zero (specified: %v)", flRPCTimeout)
 	}
+	if !flTLS && flTLSNoVerify {
+		argError("specified -tls-no-verify without specifying -tls")
+	}
+	if !flTLS && flTLSCACert != "" {
+		argError("specified -tls-ca-cert without specifying -tls")
+	}
+	if !flTLS && flTLSClientCert != "" {
+		argError("specified -tls-client-cert without specifying -tls")
+	}
+	if !flTLS && flTLSServerName != "" {
+		argError("specified -tls-server-name without specifying -tls")
+	}
+	if flTLSClientCert != "" && flTLSClientKey == "" {
+		argError("specified -tls-client-cert without specifying -tls-client-key")
+	}
+	if flTLSClientCert == "" && flTLSClientKey != "" {
+		argError("specified -tls-client-key without specifying -tls-client-cert")
+	}
+	if flTLSNoVerify && flTLSCACert != "" {
+		argError("cannot specify -tls-ca-cert with -tls-no-verify (CA cert would not be used)")
+	}
+	if flTLSNoVerify && flTLSServerName != "" {
+		argError("cannot specify -tls-server-name with -tls-no-verify (server name would not be used)")
+	}
+	log.Printf("config:")
+	log.Printf("> addr=%s conn_timeout=%v rpc_timeout=%v", flAddr, flConnTimeout, flRPCTimeout)
+	log.Printf("> tls=%v", flTLS)
+	if flTLS {
+		log.Printf("  > no-verify=%v ", flTLSNoVerify)
+		log.Printf("  > ca-cert=%s", flTLSCACert)
+		log.Printf("  > client-cert=%s", flTLSClientCert)
+		log.Printf("  > client-key=%s", flTLSClientKey)
+		log.Printf("  > server-name=%s", flTLSServerName)
+	}
+}
+
+func buildCredentials(skipVerify bool, caCerts, clientCert, clientKey, serverName string) (credentials.TransportCredentials, error) {
+	var cfg tls.Config
+
+	if clientCert != "" && clientKey != "" {
+		keyPair, err := tls.LoadX509KeyPair(clientCert, clientKey)
+		if err != nil {
+			return nil, fmt.Errorf("failed to load tls client cert/key pair. error=%v", err)
+		}
+		cfg.Certificates = []tls.Certificate{keyPair}
+	}
+
+	if skipVerify {
+		cfg.InsecureSkipVerify = true
+	} else if caCerts != "" {
+		// override system roots
+		rootCAs := x509.NewCertPool()
+		pem, err := ioutil.ReadFile(caCerts)
+		if err != nil {
+			return nil, fmt.Errorf("failed to load root CA certificates from file (%s) error=%v", caCerts, err)
+		}
+		if !rootCAs.AppendCertsFromPEM(pem) {
+			return nil, fmt.Errorf("no root CA certs parsed from file %s", caCerts)
+		}
+		cfg.RootCAs = rootCAs
+	}
+	if serverName != "" {
+		cfg.ServerName = serverName
+	}
+	return credentials.NewTLS(&cfg), nil
 }
 
 func main() {
@@ -79,17 +164,31 @@ func main() {
 		}
 	}()
 	log.Printf("establishing connection")
-	conn, err := grpc.DialContext(ctx, flAddr,
-		grpc.WithInsecure(),
+
+	opts := []grpc.DialOption{
 		grpc.WithUserAgent("grpc_health_probe"),
 		grpc.WithBlock(),
-		grpc.WithTimeout(flConnTimeout))
+		grpc.WithTimeout(flConnTimeout)}
+	if flTLS {
+		creds, err := buildCredentials(flTLSNoVerify, flTLSCACert, flTLSClientCert, flTLSClientKey, flTLSServerName)
+		if err != nil {
+			log.Printf("failed to initialize tls credentials. error=%v", err)
+			os.Exit(StatusInvalidArguments)
+		}
+		opts = append(opts, grpc.WithTransportCredentials(creds))
+	} else {
+		opts = append(opts, grpc.WithInsecure())
+	}
+	connStart := time.Now()
+	conn, err := grpc.DialContext(ctx, flAddr, opts...)
 	if err != nil {
 		log.Printf("failed to connect service at %q: %+v", flAddr, err)
 		os.Exit(StatusConnectionFailure)
 	}
+	connDuration := time.Since(connStart)
 	defer conn.Close()
 
+	rpcStart := time.Now()
 	rpcCtx, rpcCancel := context.WithTimeout(ctx, flRPCTimeout)
 	defer rpcCancel()
 	resp, err := healthpb.NewHealthClient(conn).Check(rpcCtx, &healthpb.HealthCheckRequest{})
@@ -97,10 +196,12 @@ func main() {
 		log.Printf("health check rpc failed: %+v", err)
 		os.Exit(StatusRPCFailure)
 	}
+	rpcDuration := time.Since(rpcStart)
 
 	if resp.GetStatus() != healthpb.HealthCheckResponse_SERVING {
 		log.Printf("service unhealthy (responded with %q)", resp.GetStatus().String())
 		os.Exit(StatusUnhealthy)
 	}
-	log.Printf("healthy: %v", resp.GetStatus().String())
+	log.Printf("time elapsed: connect=%v rpc=%v", connDuration, rpcDuration)
+	log.Printf("status: %v", resp.GetStatus().String())
 }
