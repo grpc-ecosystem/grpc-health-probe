5b995805d497051307fd94a65d33b5de3227c6a4
Arindam Nayak <arindam.nayak@outlook.com>
2020-05-07 18:58:04 +0530

    Added negative testcase

--- test/grpc_health_probe_test.go
+++ test/grpc_health_probe_test.go
@@ -2,6 +2,7 @@ package test
 
 import (
 	"github.com/grpc-ecosystem/grpc-health-probe/test/healthserver"
+	healthpb "github.com/grpc-ecosystem/grpc-health-probe/test/healthserver/proto"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"os/exec"
@@ -9,23 +10,37 @@ import (
 	"time"
 )
 
-func TestHealthProbe(t *testing.T) {
+func TestHealthProbeAsServing(t *testing.T) {
 	// Start the GRPC server
 	go healthserver.Start()
 
 	// Wait till the grpc server is up
 	time.Sleep(100 * time.Millisecond)
 
-	// Now execute the health probe
+	// Now execute the health probe, it should not fail as status is serving
+	testStatus(t, "SERVING", false)
+
+	// This should fail as the status is set to no serving.
+	healthserver.SetStatus(healthpb.HealthCheckResponse_NOT_SERVING)
+	testStatus(t, "NOT SERVING", true)
+
+	// Then stop the health service
+	healthserver.Stop()
+}
+
+// testStatus test the status of grpc server with expected status
+func testStatus(t *testing.T, expectedStatus string, isFail bool) {
 	tmpProcess := exec.Command(
 		"grpc-health-probe",
 		"-addr",
 		healthserver.GRPC_TEST_ADDRESS,
 	)
 	output, err := tmpProcess.CombinedOutput()
-	require.NoError(t, err)
-	assert.Contains(t, string(output), "SERVING")
+	if !isFail {
+		require.NoError(t, err, "")
+		assert.Contains(t, string(output), expectedStatus)
+	} else {
+		require.Error(t, err)
+	}
 
-	// Then stop the health service
-	healthserver.Stop()
 }
--- test/healthserver/server.go
+++ test/healthserver/server.go
@@ -13,11 +13,12 @@ type server struct {
 
 var (
 	grpcServer *grpc.Server
+	status healthpb.HealthCheckResponse_ServingStatus
 )
 const GRPC_TEST_ADDRESS = "0.0.0.0:15000"
 
 func (*server) Check(ctx context.Context, request *healthpb.HealthCheckRequest) (response *healthpb.HealthCheckResponse, err error) {
-	return &healthpb.HealthCheckResponse{Status: healthpb.HealthCheckResponse_SERVING}, nil
+	return &healthpb.HealthCheckResponse{Status: status}, nil
 }
 
 func (*server) Watch(request *healthpb.HealthCheckRequest, hs healthpb.Health_WatchServer) error {
@@ -29,11 +30,14 @@ func Start() {
 	if err != nil {
 		log.Fatalf("Error %v", err)
 	}
-
+	status = healthpb.HealthCheckResponse_SERVING
 	grpcServer = grpc.NewServer()
 	healthpb.RegisterHealthServer(grpcServer, &server{})
 	grpcServer.Serve(lis)
 }
 func Stop() {
 	grpcServer.Stop()
+}
+func SetStatus(newStatus healthpb.HealthCheckResponse_ServingStatus) {
+	status = newStatus
 }
