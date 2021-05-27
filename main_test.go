9105ff69444e2e348105eff8c5dda61a9774d737
Rodrigo Queiro <rodrigoq@google.com>
2021-05-27 19:21:23 +0200

    Add a simple test
    Since a lot of the logic is in the flag parsing and return code
    generation, it seems worth including it in the test, which requires a
    bit of a refactor.

---
+++ main_test.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"net"
+	"testing"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/health"
+	"google.golang.org/grpc/health/grpc_health_v1"
+)
+
+// startServing runs the gRPC server in a goroutine and returns the addr to
+// connect on.
+func startServing(t *testing.T, srv *grpc.Server) string {
+	t.Helper()
+	lis, err := net.Listen("tcp", "localhost:0")
+	if err != nil {
+		t.Fatalf("failed to listen: %v", err)
+	}
+	go srv.Serve(lis)
+	return lis.Addr().String()
+}
+
+func TestHealthProbe(t *testing.T) {
+	srv := grpc.NewServer()
+	grpc_health_v1.RegisterHealthServer(srv, health.NewServer())
+	addr := startServing(t, srv)
+	defer srv.Stop()
+
+	retcode := probe("-addr=" + addr)
+	if retcode != 0 {
+		t.Errorf("probe -addr=%s returned %d, want 0", addr, retcode)
+	}
+}
+
+func TestHealthProbeUnimplemented(t *testing.T) {
+	srv := grpc.NewServer()
+	addr := startServing(t, srv)
+	defer srv.Stop()
+
+	retcode := probe("-addr=" + addr)
+	if retcode != StatusRPCFailure {
+		t.Errorf("probe -addr=%s returned %d, want 0", addr, retcode)
+	}
+}
